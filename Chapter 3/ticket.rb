2e48dd3d54f8e04145e36d1fcef5974539e7ea81
Aldo Enriquez <aldoo.enriquez@gmail.com>
2023-07-26 18:42:23 -0600

    Exercise: finished chapter 3.6 exercises

--- Chapter 3/ticket.rb
+++ Chapter 3/ticket.rb
@@ -11,12 +11,15 @@ class Ticket
   end
 end
 
-th = Ticket.new("Town Hall")
-th.date = "2013-11-12"
-cc = Ticket.new("Convention Center")
-cc.date =  "2014-12-13"
-puts "We've created two tickets."
-puts "The first is for a #{th.venue} event on #{th.date}."
-puts "The second is for an event on #{cc.date} at #{cc.venue}."
-th.price = 100
-puts "The ticket for #{th.venue} has been discounted 15% to $#{"%.2f" % th.discount(15)}."
+def Ticket.most_expensive(*tickets)
+  tickets.max_by(&:price)
+end
+
+th = Ticket.new("Town Hall", "2013-11-12")
+cc = Ticket.new("Convention Center", "2014-12-13")
+fg = Ticket.new("Fairgrounds", "2015-10-11")
+th.price = 12.55
+cc.price = 10.00
+fg.price = 18.00
+highest = Ticket.most_expensive(th,cc,fg)
+puts "The highest-priced ticket is the one for #{highest.venue}."
